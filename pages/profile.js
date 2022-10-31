299d05f39207534a45191c43a327cbc47826d861
anithacuautle <cuautleanayeli3@gmail.com>
2022-10-31 15:42:23 -0600

    cam/Ana

--- pages/profile.js
+++ pages/profile.js
@@ -1,93 +1,177 @@
+import Link from "next/link";
+import Sidebar from "../components/Sidebar";
+
 export default function Home() {
     return (
-        <div>
-  <div class="md:grid md:grid-cols-3 md:gap-6">
-    <div class="md:col-span-1">
-      <div class="px-4 sm:px-0">
-        <h3 class="text-lg font-medium leading-6 text-gray-900">Profile</h3>
-        <p class="mt-1 text-sm text-gray-600">
-          This information will be displayed publicly so be careful what you share.
-        </p>
-      </div>
-    </div>
-    <div class="mt-5 md:mt-0 md:col-span-2">
-      <form action="#" method="POST">
-        <div class="shadow sm:rounded-md sm:overflow-hidden">
-          <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
-            <div class="grid grid-cols-3 gap-6">
-              <div class="col-span-3 sm:col-span-2">
-                <label for="company_website" class="block text-sm font-medium text-gray-700">
-                  Website
-                </label>
-                <div class="mt-1 flex rounded-md shadow-sm">
-                  <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
-                    http://
-                  </span>
-                  <input type="text" name="company_website" id="company_website" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="www.example.com"/>
+      <div className="h-screen flex flex-row justify-start hero"><Sidebar />
+      
+      <div class="mx-auto container max-w-1xl md:w-3/4 shadow-md -space-y-4">
+        <div class=" border-t-2 rounded-t ">
+              <img
+                class=" container  rounded-t  h-36"
+                alt="User avatar"
+                src="portada.jpg"
+              />
+              
+        </div>
+        <div class="mb-2">
+                            <img class=" -mt-8 inline-flex rounded-full" src="perfil.jpg" width="64" height="64" alt="User" />
+                        </div>
+        <div class="bg-white space-y-6">
+          <div class="md:inline-flex space-y-4 md:space-y-0 w-full p-4 text-gray-500 items-center">
+            <h2 class="md:w-1/3 max-w-sm mx-auto">Cuenta</h2>
+            <div class="md:w-2/3 max-w-sm mx-auto">
+              <div class="w-full inline-flex border">
+                <div class="pt-2 w-1/12 bg-gray-100 bg-opacity-50">
+                  <svg
+                    fill="none"
+                    class="w-6 text-gray-400 mx-auto"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                    />
+                  </svg>
+                </div>
+                <input
+                  type="email"
+                  class="w-11/12 focus:outline-none focus:text-gray-600 p-2"
+                  placeholder="email@example.com"
+                  disabled
+                />
+              </div>
+            </div>
+          </div>
+
+          <hr />
+          <div class="md:inline-flex  space-y-4 md:space-y-0  w-full p-4 text-gray-500 items-center">
+            <h2 class="md:w-1/3 mx-auto max-w-sm">Información personal</h2>
+            <div class="md:w-2/3 mx-auto max-w-sm space-y-5">
+              <div>
+                <div class="w-full inline-flex border">
+                  <div class="w-1/12 pt-2 bg-gray-100">
+                    <svg
+                      fill="none"
+                      class="w-6 text-gray-400 mx-auto"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                      />
+                    </svg>
+                  </div>
+                  <input
+                    type="text"
+                    class="w-11/12 focus:outline-none focus:text-gray-600 p-2"
+                    placeholder="Charly Olivas"
+                  />
                 </div>
               </div>
-            </div>
-
-            <div>
-              <label for="about" class="block text-sm font-medium text-gray-700">
-                About
-              </label>
-              <div class="mt-1">
-                <textarea id="about" name="about" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="you@example.com"></textarea>
-              </div>
-              <p class="mt-2 text-sm text-gray-500">
-                Brief description for your profile. URLs are hyperlinked.
-              </p>
-            </div>
-
-            <div>
-              <label class="block text-sm font-medium text-gray-700">
-                Photo
-              </label>
-              <div class="mt-1 flex items-center">
-                <span class="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                  <svg class="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
-                    <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                  </svg>
-                </span>
-                <button type="button" class="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                  Change
-                </button>
-              </div>
-            </div>
-
-            <div>
-              <label class="block text-sm font-medium text-gray-700">
-                Cover photo
-              </label>
-              <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                <div class="space-y-1 text-center">
-                  <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="True">
-                    <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                  </svg>
-                  <div class="flex text-sm text-gray-600">
-                    <label for="file-upload" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-                      <span>Upload a file</span>
-                      <input id="file-upload" name="file-upload" type="file" class="sr-only"/>
-                    </label>
-                    <p class="pl-1">or drag and drop</p>
+              <div>
+                <div class="w-full inline-flex border">
+                  <div class="pt-2 w-1/12 bg-gray-100">
+                    <svg
+                      fill="none"
+                      class="w-6 text-gray-400 mx-auto"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z"
+                      />
+                    </svg>
                   </div>
-                  <p class="text-xs text-gray-500">
-                    PNG, JPG, GIF up to 10MB
-                  </p>
+                  <input
+                    type="text"
+                    class="w-11/12 focus:outline-none focus:text-gray-600 p-2"
+                    placeholder="12341234"
+                  />
                 </div>
               </div>
             </div>
           </div>
-          <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-            <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-              Save
-            </button>
+
+          <hr />
+          <div class="md:inline-flex w-full space-y-2 md:space-y-0 p-4 text-gray-500 items-center">
+            <h2 class="md:w-4/12 max-w-sm mx-auto">Cambiar contraseña</h2>
+
+            <div class="md:w-5/12 w-full md:pl-9 max-w-sm mx-auto space-y-5 md:inline-flex pl-2">
+              <div class="w-full inline-flex border-b">
+                <div class="w-1/12 pt-2">
+                  <svg
+                    fill="none"
+                    class="w-6 text-gray-400 mx-auto"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+                    />
+                  </svg>
+                </div>
+                <input
+                  type="password"
+                  class="w-11/12 focus:outline-none focus:text-gray-600 p-2 ml-4"
+                  placeholder="***********"
+                />
+              </div>
+            </div>
+
+            <div class="md:w-3/12 text-center md:pl-6">
+              <button class="text-white w-full mx-auto max-w-sm rounded-md text-center bg-cyan-400 py-2 px-4 inline-flex items-center focus:outline-none md:float-right">
+                <svg
+                  fill="none"
+                  class="w-4 text-white mr-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+                  />
+                </svg>
+                Actualizar
+              </button>
+            </div>
+            
+          </div>
+
+          <div class="w-full p-0 text-right text-gray-500">
+            <Link href={"/initial"}><button class="inline-flex items-center focus:outline-none mr-4">
+            <svg 
+            class="h-8 w-8 text-gray-500"  
+            viewBox="0 0 24 24"  
+            fill="none"  
+            stroke="currentColor"  
+            stroke-width="2"  
+            stroke-linecap="round"  
+            stroke-linejoin="round">  
+            <path 
+            d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />  
+            <polyline 
+            points="10 17 15 12 10 7" />  
+            <line x1="15" y1="12" x2="3" y2="12" />
+            </svg>
+            </button></Link>
           </div>
         </div>
-      </form>
-    </div>
-  </div>
+      </div>
 </div>
     );
   }
