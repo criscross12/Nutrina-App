4f6559fb557652df779617e30a4186fd59d57542
CristianMorales <kristianmkf2012@hotmail.com>
2022-11-26 23:10:32 -0600

    Upload at server with apis

--- pages/patients/welcome.jsx
+++ pages/patients/welcome.jsx
@@ -1,21 +1,7 @@
-import FullCalendar from "@fullcalendar/react";
-import interactionPlugin from "@fullcalendar/interaction";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import { useRef } from "react";
 import Layout from "../../components/layout";
 
 const Calendar = () => {
-  const calendarRef = useRef(null);
-  return (
-    <Layout>
-      <FullCalendar
-        innerRef={calendarRef}
-        plugins={[timeGridPlugin, interactionPlugin]}
-        editable
-        selectable
-      />
-    </Layout>
-  );
+  return <Layout></Layout>;
 };
 
 export default Calendar;
